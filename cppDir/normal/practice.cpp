bc5854b0b587ed2ad8168a655f1076e14a63f239
Willem E. Wilcox <wilcox_we@cobra.cs.mercer.edu>
2023-11-29 21:48:55 -0500

    C++ Complete

--- cppDir/normal/practice.cpp
+++ cppDir/normal/practice.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <cstdint>
+#include <limits>
 
 int main() {
-    // this is n =  2.1 billion and 1, 3n + 1
-    // this is being used to test limits of cpp uint32
-    uint64_t myUnsignedInt64 = 6300000004;
+    uint64 myUnsignedInt64 = 6300000004;
+    unsigned long long tester = 6300000004;
 
     std::cout << "Size of myUnsignedInt64: " << sizeof(myUnsignedInt64) << " bytes" << std::endl;
-   std::cout << "Contents of myUnsignedInt64: " << myUnsignedInt64 << " ..." << std::endl;
+    std::cout << "Contents of myUnsignedInt64: " << myUnsignedInt64 << " ..." << std::endl;
+    std::cout << "Maximum value of uint64_t: " << std::numeric_limits<uint64>::max() << std::endl;
+
+    std::cout << "Size of u L L: " << sizeof(tester) << " bytes" << std::endl;
+    std::cout << "Contents of u L L: " << tester << " ..." << std::endl;
+    std::cout << "Maximum value of unsigned long long: " << std::numeric_limits<unsigned long long>::max() << std::endl;
+
     return 0;
 }
